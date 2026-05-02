b3d64b9b06ad3e502647322a30c47bf65792026c
Gaetano-Leanza <gaetano1981@live.de>
2026-05-02 16:10:02 +0200

    work on views.py

--- auth_app/views.py
+++ auth_app/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import permissions, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+class RegistrationView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        repeated_password = request.data.get('repeated_password')
+        fullname = request.data.get('fullname')
+
+        if password != repeated_password:
+            return Response({'error': 'Passwörter stimmen nicht überein.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if User.objects.filter(username=email).exists():
+            return Response({'error': 'Ein User mit dieser E-Mail existiert bereits.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = User.objects.create_user(
+            username=email, email=email, password=password)
+        user.first_name = fullname
+        user.save()
+
+        token, created = Token.objects.get_or_create(user=user)
+
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email,
+            'fullname': user.first_name
+        }, status=status.HTTP_201_CREATED)
