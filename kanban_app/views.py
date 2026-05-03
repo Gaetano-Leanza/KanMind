0d8ecf2f9562eee76377b22e92a21e489b8a34f3
Gaetano-Leanza <gaetano1981@live.de>
2026-05-03 21:33:45 +0200

    add comments to views.py

--- kanban_app/views.py
+++ kanban_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+# --- Template Views ---
+# This file is typically used for standard Django HTML template views.
+# For this project, API logic is located in the 'api/' directory.
+
 # Create your views here.
