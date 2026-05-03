7a255466ee5a53a36c1620382cd7fffae6718fbb
Gaetano-Leanza <gaetano1981@live.de>
2026-05-03 12:43:38 +0200

    fix url.py

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
 from .views import LoginView, RegistrationView
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='auth-login'),
-    path('registration/', RegistrationView.as_view(), name='auth-registration'),
+    path('auth/login/', LoginView.as_view(), name='auth-login'),
+    path('auth/registration/', RegistrationView.as_view(),
+         name='auth-registration'),
 ]
--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('auth_app.api.urls')),
-    path('api/kanban/', include('kanban_app.api.urls')),
+    path('kanban/', include('kanban_app.api.urls')),
 ]
+
