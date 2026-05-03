b21cf14df39db73aff19b95afde75a32f2f2f3dd
Gaetano-Leanza <gaetano1981@live.de>
2026-05-03 20:39:33 +0200

    add comments to apps.py

--- kanban_app/apps.py
+++ kanban_app/apps.py
@@ -1,5 +1,15 @@
 from django.apps import AppConfig
 
+# --- Application Configuration ---
 class KanbanAppConfig(AppConfig):
+    """
+    Configuration class for the 'kanban_app'.
+    This class is used by Django to configure the application's behavior
+    and metadata within the project.
+    """
+    
+    # Sets the default type for primary keys (auto-incrementing 64-bit integer)
     default_auto_field = 'django.db.models.BigAutoField'
+    
+    # The full Python path to the application
     name = 'kanban_app'
