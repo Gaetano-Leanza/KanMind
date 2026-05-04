3617379b3a63d97fa6fd31c2357326d141e26d1d
Gaetano-Leanza <gaetano1981@live.de>
2026-05-04 23:57:49 +0200

    add comments to admin.py

--- auth_app/admin.py
+++ auth_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+# --- Admin Site Registration ---
+# The standard Django User model is registered by default.
+# Custom authentication-related models should be registered here.
+
 # Register your models here.
